4433e38c6df42b3be2d811e000ea8f376c6de759
OTABOY RAKHMONOV <120033884+dad0586@users.noreply.github.com>
2023-10-19 16:05:59 +0500

    1st

---
+++ src/PhoneBook.java
@@ -0,0 +1,10 @@
+public class PhoneBook {
+    public String name;
+    public PhoneBook(String name){
+        this.name =name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
